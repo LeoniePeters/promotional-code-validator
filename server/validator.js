70f1590bad38db167ec1c21b64c68b5ba1c228b1
LeoniePeters <leoniepeters@gmail.com>
2019-04-03 08:39:45 +0200

    Add divisible by 11 check to validatePromotionCode

--- server/validator.js
+++ server/validator.js
@@ -1,12 +1,15 @@
 const validatePromotionCode = (code) => {
-    const array = code.toString().split('').map(num => Number(num));
+  const array = code.toString().split('').map(num => Number(num));
+  const reducer = (acc, val, currentIndex) => { return acc + val * (9 - currentIndex) };
+  console.log('array', array)
+  console.log('reducer', array.reduce(reducer, 0))
 
-    if (array.length === 9) {
-        return true
-    } 
-   return false
-    
-
+  if (array.length === 9) {
+    if(array.reduce(reducer, 0) % 11 === 0) {
+      return true
+    }
+  }
+  return false
 }
 
 module.exports = { validatePromotionCode }
