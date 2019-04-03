800d1ee5991bb1038385131bca552b70aaf09e90
LeoniePeters <leoniepeters@gmail.com>
2019-04-03 10:26:03 +0200

    Complete endpoint with validatePromotionCode

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
+const { validatePromotionCode } = require('./validator')
 
 router.post('/promocode', (req, res) => {
   console.log(req.body)
@@ -8,6 +9,10 @@ router.post('/promocode', (req, res) => {
     res.status(400).send({ message: 
       'The code you provided is invalid. Make sure it contains 9 numbers'});
   } 
+  if(!validatePromotionCode(promoCode)) {
+    res.status(400).send({ message: 
+      'The code you provided is invalid.'});
+  }
   res.status(200).send({message: 'Valid code, but not the lucky one.'});
 })
 
